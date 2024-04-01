a09549e24c01618c638aa4e7c5c938af646fbac9
0xPCDefenders <abstractlabz@proton.me>
2024-04-01 11:54:40 -0400

    integrating content for analysis page

--- src/components/combobox.tsx
+++ src/components/combobox.tsx
@@ -1,10 +1,12 @@
+// Assuming you're using Next.js and have installed `js-cookie`
 "use client"
 
 import * as React from "react"
+import Cookies from 'js-cookie'
 import { Check, ChevronsUpDown } from "lucide-react"
 import Image from "next/image"
 import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/button"
 import {
   Command,
   CommandEmpty,
@@ -18,79 +20,110 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover"
 import { frameworks } from "../data/tickerslist"
-import { Icon } from "@radix-ui/react-select";
 
-export function Combobox() {
+// Add a new prop to accept the function for setting selected ticker
+export function Combobox({ setSelectedTicker }) {
   const [open, setOpen] = React.useState(true)
   const [value, setValue] = React.useState("")
+  const [watchlist, setWatchlist] = React.useState([]);
+
+  React.useEffect(() => {
+    const watchlistCookie = Cookies.get('watchlist');
+    if (watchlistCookie) {
+      setWatchlist(JSON.parse(watchlistCookie));
+    }
+  }, []);
+
+  const saveWatchlistToCookies = (watchlist) => {
+    Cookies.set('watchlist', JSON.stringify(watchlist), { expires: 365 });
+  };
+
+  const addToWatchlist = (newValue) => {
+    setWatchlist((currentWatchlist) => {
+      const updatedWatchlist = [...new Set([...currentWatchlist, newValue])];
+      saveWatchlistToCookies(updatedWatchlist);
+      return updatedWatchlist;
+    });
+  };
+
+  const removeFromWatchlist = (valueToRemove) => {
+    setWatchlist((currentWatchlist) => {
+      const updatedWatchlist = currentWatchlist.filter(item => item !== valueToRemove);
+      saveWatchlistToCookies(updatedWatchlist);
+      return updatedWatchlist;
+    });
+  };
+
+  // Update the setValue function to also call setSelectedTicker
+  const handleSelect = (currentValue) => {
+    const newValue = currentValue === value ? "" : currentValue;
+    setValue(newValue);
+    setOpen(open);
+    // Call the function passed through props to set the selected ticker in the parent component
+    setSelectedTicker(newValue);
+  };
 
   return (
     <Popover open={open}>
-  <PopoverTrigger asChild>
-    <Button
-      variant="outline"
-      role="combobox"
-      aria-expanded={open}
-      className="w-[200px] justify-between bg-accent-color z-20"
-    >
-      {value
-        ? frameworks.find((framework) => framework.value === value)?.label
-        : <p className="text-black">Select Ticker...</p>}
-      <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-    </Button>
-  </PopoverTrigger>
-  <PopoverContent className="w-[200px] p-0 bg-accent-color overflow-auto max-h-[385px]">
-    <Command className="bg-accent-color">
-      <CommandInput className="w-full bg-accent-color text-black" placeholder="Enter a ticker symbol..." />
-      <CommandEmpty>No companies found.</CommandEmpty>
-      <CommandGroup heading="Suggestions">
-        {frameworks.map((framework) => (
-          <div key={framework.value} className="w-full flex items-center justify-between">
-            <CommandItem
-              value={framework.value}
-              className="text-black flex-1"
-              onSelect={(currentValue) => {
-                setValue(currentValue === value ? "" : currentValue);
-                setOpen(open);
-              }}
-            >
-              <Check
-                className={cn(
-                  "mr-2 h-4 w-4",
-                  value === framework.value ? "opacity-100" : "opacity-0"
-                )}
-              />
-              {framework.label}
-            </CommandItem>
-            <Image className="pr-2" src="/plus.png" width={25} height={25} alt="+" onClick={() => {}} />
-          </div>
-        ))}
-      </CommandGroup>
-      <CommandGroup heading="Watchlist">
-        {frameworks.map((framework) => (
-          <div key={framework.value} className="w-full flex items-center justify-between">
-            <CommandItem
-              value={framework.value}
-              className="text-black flex-1"
-              onSelect={(currentValue) => {
-                setValue(currentValue === value ? "" : currentValue);
-                setOpen(open);
-              }}
-            >
-              <Check
-                className={cn(
-                  "mr-2 h-4 w-4",
-                  value === framework.value ? "opacity-100" : "opacity-0"
-                )}
-              />
-              {framework.label}
-            </CommandItem>
-          </div>
-        ))}
-      </CommandGroup>
-    </Command>
-  </PopoverContent>
-</Popover>
-
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          className="w-[200px] justify-between bg-accent-color z-20"
+        >
+          {value
+            ? frameworks.find((framework) => framework.value === value)?.label
+            : <p className="text-black">Select Ticker...</p>}
+          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[200px] p-0 bg-accent-color overflow-auto max-h-[385px]">
+        <Command className="bg-accent-color">
+          <CommandInput className="w-full bg-accent-color text-black" placeholder="Enter a ticker symbol..." />
+          <CommandEmpty>No companies found.</CommandEmpty>
+          <CommandGroup heading="Suggestions">
+            {frameworks.map((framework) => (
+              <div key={framework.value} className="w-full flex items-center justify-between">
+                <CommandItem
+                  value={framework.value}
+                  className="text-black flex-1"
+                  onSelect={() => handleSelect(framework.value)}
+                >
+                  <Check
+                    className={cn(
+                      "mr-2 h-4 w-4",
+                      value === framework.value ? "opacity-100" : "opacity-0"
+                    )}
+                  />
+                  {framework.label}
+                </CommandItem>
+                <Image className="pr-2" src="/plus.png" width={25} height={25} alt="add" onClick={() => addToWatchlist(framework.value)} />
+              </div>
+            ))}
+          </CommandGroup>
+          <CommandGroup heading="Watchlist">
+            {frameworks.filter(framework => watchlist.includes(framework.value)).map((framework) => (
+              <div key={framework.value} className="w-full flex items-center justify-between">
+                <CommandItem
+                  value={framework.value}
+                  className="text-black flex-1"
+                  onSelect={() => handleSelect(framework.value)}
+                >
+                  <Check
+                    className={cn(
+                      "mr-2 h-4 w-4",
+                      value === framework.value ? "opacity-100" : "opacity-0"
+                    )}
+                  />
+                  {framework.label}
+                </CommandItem>
+                <Image className="pr-2" src="/minus.png" width={30} height={30} alt="remove" onClick={() => removeFromWatchlist(framework.value)} />
+              </div>
+            ))}
+          </CommandGroup>
+        </Command>
+      </PopoverContent>
+    </Popover>
   )
 }
--- src/components/deck.tsx
+++ src/components/deck.tsx
@@ -13,32 +13,52 @@ interface UserProfile {
   email: string;
   credits: number;
   is_member: boolean;
-  
   // Add other user profile fields as needed
 }
 
-const Deck: React.FC = () => {
+interface StockAnalysis {
+  Ticker: string;
+  StockPerformance: string;
+  FinancialHealth: string;
+  NewsSummary: string;
+  CompanyDesc: string;
+  TechnicalAnalysis: string;
+}
 
+const Deck: React.FC = () => {
   const [profile, setProfile] = useState<UserProfile | null>(null);
+  const [analysis, setAnalysis] = useState<StockAnalysis | null>(null);
+  const [selectedTicker, setSelectedTicker] = useState<string>(''); // Placeholder for selected ticker value
 
   useEffect(() => {
-      const savedProfile = Cookies.get('userProfile');
-      if (savedProfile) {
-          setProfile(JSON.parse(savedProfile) as UserProfile);
-      }
+    const savedProfile = Cookies.get('userProfile');
+    if (savedProfile) {
+        setProfile(JSON.parse(savedProfile) as UserProfile);
+    }
   }, []);
+
+  const handleAnalysis = async () => {
+    // Placeholder URL - adjust to your backend API endpoint
+    const apiUrl = `https://data.fineasapp.io:8443/ret?ticker=${selectedTicker}`;
+    try {
+      const response =  fetch(apiUrl);
+      const data = (await response).json();
+      console.log('Stock analysis:', data);
+      setAnalysis(await data);
+    } catch (error) {
+      console.error('Error fetching stock analysis:', error);
+    }
+  };
 
   return (
     <div className="flex justify-center items-center md:p-4 sm:p-1 pt-6">
       <Card className="glowing-border p-1 border shadow-xl w-full max-w-[230vh] bg-main-color overflow-hidden relative" style={{ minHeight: '5vh' }}>
-      <CardContent className="flex flex-col items-start space-y-8 2xl:space-y-0 lg:flex-row 2xl:items-start relative" style={{ gap: '25px' }}>
-          {/* Combobox and Generate Analysis button grouping, adjusted for vertical spacing */}
+        <CardContent className="flex flex-col items-start space-y-8 2xl:space-y-0 lg:flex-row 2xl:items-start relative" style={{ gap: '25px' }}>
           <div className="w-full lg:w-1/4 flex justify-center items-center flex-col mb-6 relative">
-            <Combobox />
-            <Button className="w-full mt-[435px] self-start bg-blue-700">Generate Analysis</Button>
+            <Combobox setSelectedTicker={setSelectedTicker} /> {/* Adjust this to correctly set the selectedTicker */}
+            <Button className="w-full mt-[435px] self-start bg-blue-700" onClick={handleAnalysis}>Generate Analysis</Button>
           </div>
 
-          {/* Tabs */}
           <div className="w-full md:flex-grow mb-6 flex justify-center">
             <Tabs defaultValue="price" className="w-full">
               <TabsList className='w-full flex justify-center'>
@@ -47,17 +67,15 @@ const Deck: React.FC = () => {
                 <TabsTrigger className='text-xs md:text-lg' value="technical">Technicals</TabsTrigger>
                 <TabsTrigger className='text-xs md:text-lg' value="financials">Financials</TabsTrigger>
               </TabsList>
-              <TabsContent className='text-white' value="price">Price Information</TabsContent>
-              <TabsContent className='text-white' value="news">News Information</TabsContent>
-              <TabsContent className='text-white' value="technical">Technical Analysis Details</TabsContent>
-              <TabsContent className='text-white' value="financials">Financial Details</TabsContent>
+              <TabsContent className='text-white' value="price">{analysis?.StockPerformance}</TabsContent>
+              <TabsContent className='text-white' value="news">{analysis?.NewsSummary}</TabsContent>
+              <TabsContent className='text-white' value="technical">{analysis?.TechnicalAnalysis}</TabsContent>
+              <TabsContent className='text-white' value="financials">{analysis?.FinancialHealth}</TabsContent>
             </Tabs>
           </div>
 
-          {/* CandleChart, adjusted for vertical spacing */}
           <div className="w-full md:flex-grow mb-6 relative">
             <CandleChart />
-            {/* Credits positioned at the same level as the Chart */}
           </div>
           <div className='absolute bottom-0 right-4 m-12 text-s text-white-400'>
             <p className='text-white'>Credits Available: {profile?.credits}</p>

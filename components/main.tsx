db6bb9107a8b7422e6fe825b13606d270e774e39
0xPCDefenders <abstractlabz@proton.me>
2024-09-17 23:16:29 -0400

    new commit

--- components/main.tsx
+++ components/main.tsx
@@ -5,30 +5,77 @@ import FAQs from "./f-a-qs";
 import SignUp from "./sign-up";
 import Discord from "./discord";
 
-const Main: NextPage = () => {
+export type MainType = {
+  className?: string;
+};
+
+const Main: NextPage<MainType> = ({ className = "" }) => {
   return (
-    <div className="w-full flex flex-col items-center justify-start text-white">
-      <nav className="w-full flex justify-between items-center p-6">
-        <img
-          className="h-11 w-40 object-cover"
-          alt="Logo"
-          src="/white-logo--no-background-1@2x.png"
-        />
-        <div className="flex space-x-6">
-          <Home1 />
-          <About1 />
-          <FAQs />
-          <Discord />
-          <SignUp />
+    <div
+      className={`w-[629px] flex flex-row items-start justify-start relative max-w-full text-left text-base text-white font-public-sans ${className}`}
+    >
+      <img
+        className="h-[436px] w-[681px] absolute !m-[0] bottom-[-35px] left-[-557px] object-cover z-[4]"
+        alt=""
+        src="/component-1@2x.png"
+      />
+      <div className="h-[975px] w-[1153px] absolute !m-[0] bottom-[-370px] left-[-651px]">
+        <div className="absolute top-[321px] left-[499px] rounded-[50%] [background:radial-gradient(50%_50%_at_50%_50%,_rgba(31,_85,_255,_0.25),_rgba(31,_85,_255,_0))] w-[654px] h-[654px] z-[1]" />
+        <div className="absolute top-[0px] left-[0px] w-[589px] h-[823px]">
+          <div className="absolute top-[-100px] left-[-334px] rounded-[50%] [background:radial-gradient(50%_50%_at_50%_50%,_rgba(217,_31,_255,_0.15),_rgba(217,_31,_255,_0))] w-[923px] h-[923px]" />
+          <img
+            className="absolute top-[28px] left-[46px] w-[165px] h-11 object-cover z-[4]"
+            loading="lazy"
+            alt=""
+            src="/white-logo--no-background-1@2x.png"
+          />
         </div>
-      </nav>
-      <div className="flex flex-col items-center text-center px-4">
-        <h1 className="text-4xl font-bold mb-4">
-          Welcome to Fineas.ai
-        </h1>
-        <p className="text-xl max-w-2xl">
-          Your AI edge. Play the game, change the rules.
-        </p>
+      </div>
+      <div className="flex-1 flex flex-row items-start justify-start flex-wrap content-start pt-0 px-0 pb-[105px] box-border gap-[30px] max-w-full mq750:pb-[68px] mq750:box-border">
+        <div className="flex flex-col items-start justify-start pt-[41px] px-0 pb-0">
+          <Home1 />
+        </div>
+        <div className="flex flex-col items-start justify-start pt-[41px] px-0 pb-0 text-silver">
+          <About1 />
+        </div>
+        <div className="h-[500px] flex-1 relative min-w-[309px] max-w-full">
+          <FAQs propTop="39px" propLeft="0px" />
+          <div className="absolute top-[0px] left-[36px] w-[439px] h-[500px] flex flex-col items-end justify-start py-7 pl-[35px] pr-[43px] box-border max-w-full">
+            <div className="w-[654px] h-[654px] absolute !m-[0] top-[-154px] right-[-215px] rounded-[50%] [background:radial-gradient(50%_50%_at_50%_50%,_rgba(31,_85,_255,_0.25),_rgba(31,_85,_255,_0))] z-[2]" />
+            <SignUp
+              propWidth="unset"
+              propTextDecoration="none"
+              propFontWeight="700"
+              propDisplay="inline-block"
+              propMinWidth="62px"
+              signUpPosition="relative"
+              signUpTop="unset"
+              signUpLeft="unset"
+              signUpHeight="unset"
+              signUpDisplay="flex"
+              signUpFlexDirection="row"
+              signUpPadding="12px 26px"
+              rectangleDivTop="0px"
+              rectangleDivRight="0px"
+              rectangleDivBottom="0px"
+              rectangleDivLeft="0px"
+              rectangleDivMargin="0 !important"
+              signUpPosition1="relative"
+              signUpHeight1="unset"
+              signUpWidth="unset"
+              signUpTop1="unset"
+              signUpLeft1="unset"
+              signUpFlex="1"
+            />
+            <header className="self-stretch flex flex-row items-start justify-start mt-[-44px]">
+              <Discord
+                propFlex="unset"
+                propWidth="221px"
+                icbaselineDiscord="/icbaselinediscord.svg"
+              />
+            </header>
+          </div>
+        </div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,47 +1,47 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */ 
 module.exports = {
-  content: [
+  "content": [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
   ],
-  theme: {
-    extend: {
-      colors: {
-        midnightblue: {
+  "theme": {
+    "extend": {
+      "colors": {
+        "midnightblue": {
           "100": "#230f59",
-          "200": "#080733",
+          "200": "#080733"
         },
-        mediumblue: "#672bff",
-        white: "#fff",
-        indigo: {
+        "mediumblue": "#672bff",
+        "white": "#fff",
+        "indigo": {
           "100": "#411d9e",
-          "200": "#371885",
+          "200": "#371885"
         },
-        fuchsia: "#d91fff",
-        blueviolet: "#5624d4",
-        darkblue: "#481eb3",
-        mediumslateblue: "#5865f2",
-        silver: "#bfbfbf",
-        darkgray: "#9f9f9f",
+        "fuchsia": "#d91fff",
+        "blueviolet": "#5624d4",
+        "darkblue": "#481eb3",
+        "mediumslateblue": "#5865f2",
+        "silver": "#bfbfbf",
+        "darkgray": "#9f9f9f"
       },
-      spacing: {},
-      fontFamily: {
+      "spacing": {},
+      "fontFamily": {
         "public-sans": "'Public Sans'",
-        sarabun: "Sarabun",
-        inter: "Inter",
-        quicksand: "Quicksand",
+        "sarabun": "Sarabun",
+        "inter": "Inter",
+        "quicksand": "Quicksand"
       },
-      borderRadius: {
+      "borderRadius": {
         "14xl-5": "33.5px",
         "31xl-5": "50.5px",
-        "21xl": "40px",
-      },
+        "21xl": "40px"
+      }
     },
-    fontSize: {
-      base: "16px",
-      sm: "14px",
-      mini: "15px",
-      xl: "20px",
+    "fontSize": {
+      "base": "16px",
+      "sm": "14px",
+      "mini": "15px",
+      "xl": "20px",
       "53xl": "72px",
       "24xl": "43px",
       "39xl": "58px",
@@ -49,32 +49,32 @@ module.exports = {
       "10xl": "29px",
       "19xl": "38px",
       "5xl": "24px",
-      lgi: "19px",
+      "lgi": "19px",
       "13xl": "32px",
       "7xl": "26px",
       "45xl": "64px",
       "32xl": "51px",
-      inherit: "inherit",
+      "inherit": "inherit"
     },
-    screens: {
-      mq1225: {
-        raw: "screen and (max-width: 1225px)",
+    "screens": {
+      "mq1225": {
+        "raw": "screen and (max-width: 1225px)"
       },
-      lg: {
-        max: "1200px",
+      "lg": {
+        "max": "1200px"
       },
-      mq1050: {
-        raw: "screen and (max-width: 1050px)",
+      "mq1050": {
+        "raw": "screen and (max-width: 1050px)"
       },
-      mq750: {
-        raw: "screen and (max-width: 750px)",
+      "mq750": {
+        "raw": "screen and (max-width: 750px)"
       },
-      mq450: {
-        raw: "screen and (max-width: 450px)",
-      },
-    },
+      "mq450": {
+        "raw": "screen and (max-width: 450px)"
+      }
+    }
   },
-  corePlugins: {
-    preflight: false,
-  },
-};
+  "corePlugins": {
+    "preflight": false
+  }
+}
